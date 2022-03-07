50ab1fa128c69bb3d5b271c0617dd292f8a110da
trong <trongtrong099@gmail.com>
2022-03-07 21:32:49 +0700

    Screen-1-Lab3

---
+++ app/src/main/java/com/example/btthuchanhdidong/screen1lab3a.java
@@ -0,0 +1,14 @@
+package com.example.btthuchanhdidong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen1lab3a extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen1lab3a);
+    }
+}
