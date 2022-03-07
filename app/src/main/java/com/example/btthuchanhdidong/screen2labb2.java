7c607192465cbbfee1ca463f1c702686ef79ce54
trong <trongtrong099@gmail.com>
2022-03-07 20:47:43 +0700

    Screen-3-Lab2

---
+++ app/src/main/java/com/example/btthuchanhdidong/screen2labb2.java
@@ -0,0 +1,14 @@
+package com.example.btthuchanhdidong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class screen2labb2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2labb2);
+    }
+}
